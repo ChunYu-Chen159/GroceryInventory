922c7b1d2a7deab88c4bdd5b915b5b0c2fb26ac7
i11007020 <i11007020@gmail.com>
2020-05-27 23:58:05 +0800

    commit

--- src/main/java/groceryinventory/GroceryInventoryController.java
+++ src/main/java/groceryinventory/GroceryInventoryController.java
@@ -19,6 +19,7 @@ public class GroceryInventoryController {
 	@Autowired
 	OrderingInterface orderingInterface;
 	
+/*
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
@@ -26,6 +27,7 @@ public class GroceryInventoryController {
     {
 		return "success";
     }
+*/
 
 /*	// 模擬404
 	@ApiOperation(value = "模擬404", notes = "回傳404")
